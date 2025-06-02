327e7e2f522589a17eb4fe348412d526e1b56075
karemsilveriofa <karemsilverio.facu@gmail.com>
2025-06-01 21:11:38 -0300

    Delete conf.py

--- conf.py
+++
@@ -1,8 +0,0 @@
-# Token do bot Telegram
-TELEGRAM_TOKEN = "7959177410:AAF8Im5jWsBQhSZfJh23lWQlw_CTYYvpzDo"
-
-# ID do seu chat pessoal no Telegram
-TELEGRAM_CHAT_ID = 6821521589
-
-# API Key Twelve Data
-API_KEY = "c95f42c34f934f91938f91e5cc8604a6"
